e68ee51cf4a2dfefeccbfc8ed6a14fa23f3db8dc
Gustavo Vitorino <gustavo.vittorino@climatempo.com.br>
2016-11-07 17:33:53 -0200

    atualizando

--- public/script/usuctrl.js
+++ public/script/usuctrl.js
@@ -1,4 +1,19 @@
 angular.module("socialize").controller("usuctrl", function ($scope,$http) {
 	$scope.app = "socialize";
+	$scope.usuario = [];
+
+
+	$scope.adicionarUsuario = function (usuario) {
+		usuario.tipo = verificaTipoMarcado();
+		$http.post("", {nome:usuario.nome, email: usuario.email, senha: usuario.senha})
+		.success(function (data) {
+			delete $scope.usuario;
+			$scope.salvo = true;
+			$scope.cadastraUsuario.$setPristine();
+		}).error(function (data) {
+			$scope.erro = true;
+			$scope.message = "Aconteceu um problema: " + data;
+		});
+	};
 						
 });
