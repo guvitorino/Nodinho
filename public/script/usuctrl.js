8f2911aff1cd82a521e9bffe57ae2324f351ff23
Gustavo Vitorino <gustavo.vittorino@climatempo.com.br>
2016-11-09 14:41:00 -0200

    implementação find

--- public/script/usuctrl.js
+++ public/script/usuctrl.js
@@ -5,12 +5,24 @@ angular.module("socialize").controller("usuctrl", function ($scope,$http) {
 	$scope.adicionarUsuario = function () {
 
 		
-		$http.post("http://127.0.0.1:3000/usuario/salvar",{params:{"usuario":$scope.usuarioform}})
+		$http.post("usuario/salvar",{params:{"usuario":$scope.usuarioform}})
 		.success(function (data) {
 			delete $scope.usuarioform;
 			alert(data);
 			$scope.salvo = true;
 			$scope.cadastraUsuario.$setPristine();
+		}).error(function (data) {
+			$scope.erro = true;
+			$scope.message = "Aconteceu um problema: " + data;
+		});
+	};
+
+	$scope.entrar = function () {
+		$http.post("usuario/login",{params:{"usuario":$scope.loginform}})
+		.success(function (data) {
+			delete $scope.loginform;
+			console.log(data);
+			$scope.loginUsuario.$setPristine();
 		}).error(function (data) {
 			$scope.erro = true;
 			$scope.message = "Aconteceu um problema: " + data;
--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.get("/script/usu",function (req, res){
 
 
 app.post("/usuario/salvar",function (req, res){
-	console.log(req.body.params.usuario);
+	//console.log(req.body.params.usuario);
 	usuario = req.body.params.usuario;
-	
+
 	if(usuario.nome == null){
 		console.error("Ocorreu algum problema");
   		res.status(500).send('Acontenceu algum problema!');
@@ -58,6 +58,34 @@ app.post("/usuario/salvar",function (req, res){
 	}
 })
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Listening on http://localhost:' + (process.env.PORT || 3000));
+app.post("/usuario/login",function (req, res){
+	usuario = req.body.params.usuario;
+	console.log(usuario);
+	if(usuario.email == null){
+		console.error("Ocorreu algum problema");
+  		res.status(500).send('Acontenceu algum problema!');
+  	}else{
+		MongoClient.connect(url, function (err, db) {
+		  if (err){
+		  	console.error(err.stack);
+	  		res.status(500).send('Acontenceu algum problema!');
+		  }else {
+		    var collection = db.collection('usuarios');
+
+		    collection.findOne({email: usuario.email}, function(err, document) {
+			  if (err){
+		      	console.error("Ocorreu algum problema");
+	  			res.status(500).send('Acontenceu algum problema!');
+		      }else res.status(200).json({id: document._id,nome: document.nome });
+			});
+
+		    //Fecha a conexão
+		    db.close();
+		  }
+		});
+	}
+})
+
+app.listen(process.env.PORT || 8000, function () {
+    console.log('Listening on http://localhost:' + (process.env.PORT || 8000));
 });
